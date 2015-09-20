e95434bf7918aafb2f91e945641122dffbe31a43
ahugeat <hugeat.arthur@gmail.com>
2015-09-20 17:43:53 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Son of Mars
+
+### Requierement
+This project needs :
+- CMake > 2.8
+- SFML > 2.1
+- C++ 11
+- Boost FileSystem Lib
+- Box2D
+
+### Compilation
+To compile the project, execute these commands:
+
+    cd path/to/project
+    mkdir build
+    cd build
+    cmake ../src
+    make
---
+++ src/son-of-mars.cc
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 OFNI
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "local/SFMLDebugDraw.h"
+
+int main(void) {
+	sf::RenderWindow window({800, 600}, "Son of Mars");
+
+	// Create the world physic
+	b2World b2_world(b2Vec2(0.0f, 10.0f));
+
+	// Debug Box2D
+	SFMLDebugDraw debugDraw(window);
+	b2_world.SetDebugDraw(&debugDraw);
+	debugDraw.SetFlags(b2Draw::e_shapeBit);
+
+	sf::Clock clock;
+	float dt = 0.0f;
+
+	while (window.isOpen()) {
+	sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		// update
+		dt = clock.restart().asSeconds();
+		b2_world.Step(dt, 8, 3);
+
+		// render
+		window.clear(sf::Color::White);
+
+		b2_world.DrawDebugData();
+
+		window.display();
+	}
+
+	return 0;
+}
