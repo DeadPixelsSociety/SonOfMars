30bc4ac6ad7dc369951b78e10659cf2163067b04
ahugeat <hugeat.arthur@gmail.com>
2015-09-21 07:57:23 +0200

    Add the game skull.

---
+++ src/local/Entity.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015 OFNI
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef GAME_ENTITY_H
+#define GAME_ENTITY_H
+
+#include <SFML/Graphics.hpp>
+
+class Entity {
+public:
+	Entity(int priority = 0)
+	: m_priority(priority)
+	, m_alive(true) {
+
+	}
+
+	virtual ~Entity();
+
+	int getPriority() const {
+		return m_priority;
+	}
+
+	bool isAlive() const {
+		return m_alive;
+	}
+
+	void kill() {
+		m_alive = false;
+	}
+
+	virtual void update(const float dt);
+	virtual void render(sf::RenderWindow& window);
+
+private:
+	const int m_priority;
+	bool m_alive;
+};
+
+
+#endif // GAME_ENTITY_H
