cddcdf9303c9c67a45ed406dd3d24dac4831b6ec
ahugeat <hugeat.arthur@gmail.com>
2015-10-01 19:39:56 +0200

    Add Arena class.

---
+++ src/local/Arena.cc
@@ -0,0 +1,22 @@
+#include "Arena.h"
+
+Arena::Arena(b2World &b2_world) {
+    /*b2Vec2 vertices[8];
+
+    vertices[0].Set(- half_w, X * half_h);
+    vertices[1].Set(- X * half_w, half_h);
+    vertices[2].Set(  X * half_w, half_h);
+    vertices[3].Set(  half_w, X * half_h);
+    vertices[4].Set(  half_w, - X * half_h);
+    vertices[5].Set(  X * half_w, - half_h);
+    vertices[6].Set(- X * half_w, - half_h);
+    vertices[7].Set(- half_w, - X * half_h);*/
+}
+
+void Arena::update(const float dt) {
+
+}
+
+void Arena::render(sf::RenderWindow& window) {
+    //
+}
---
+++ src/local/Arena.h
@@ -0,0 +1,20 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <Box2D/Box2D.h>
+#include "Entity.h"
+
+static constexpr float BOX2D_SCALE = 0.01f;
+
+class Arena: public Entity {
+    public:
+        Character(b2World &b2_world);
+
+        virtual void update(const float dt) override;
+        virtual void render(sf::RenderWindow& window) override;
+        
+    private:
+        b2Body *m_body; 
+
+};
+#endif //ARENA_H
