6b4a65dc24793a7e0270d82307973ec191c3cb14
ahugeat <hugeat.arthur@gmail.com>
2015-10-20 10:21:25 +0200

    Add licence to the project.

--- src/local/Game.h
+++ src/local/Game.h
@@ -1,7 +1,7 @@
 /*
- * Lux
+ * Son of Mars
  *
- * Copyright (c) 2015, Julien Bernard
+ * Copyright (c) 2015-2016, Team Son of Mars
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
